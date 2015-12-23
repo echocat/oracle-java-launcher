5e814c1167a4c7529cce55411998df9a1e3a8417
Gregor Noczinski <gregor@noczinski.eu>
2015-12-23 16:08:09 +0100

    Build script...

--- make.sh
+++ make.sh
@@ -1,22 +1,31 @@
 #!/usr/bin/env bash
 set -e
 
+export GOPATH=~/go
+export PATH="${PATH}:${GOPATH}/bin"
+
 ## Create target directory
 mkdir -p target/root/usr/bin
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
-## Build files
-GOOS=linux GOARCH=386 go build -o target/root/usr/bin/orachle-java-launcher launcher.go
+## Download cross compiler
+go get github.com/laher/goxc
 if [ $? -ne 0 ]; then
     exit 1
 fi
 
-GOOS=linux GOARCH=386 go build -o target/root/usr/bin/orachle-java-launcher-installer installer.go
+goxc -bc="linux,!arm windows darwin"
 if [ $? -ne 0 ]; then
     exit 1
 fi
+
+## Build files
+#GOOS=linux GOARCH=386 go build -o target/root/usr/bin/orachle-java-launcher main.go
+#if [ $? -ne 0 ]; then
+#    exit 1
+#fi
 
 cd target/root
 if [ $? -ne 0 ]; then
